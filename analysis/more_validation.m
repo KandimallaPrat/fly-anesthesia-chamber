251cb353681af02058d2bfee313753dc927a61f6
Jon Kenny <jkenny@caltech.edu>
2021-07-05 21:17:54 -0700

    Figures

--- analysis/more_validation.m
+++ analysis/more_validation.m
@@ -13,7 +13,7 @@ clc; clear variables; close all;
 
 datadir = '/local/anesthesia/data/';
 
-for q = 3:4
+for q = 1
     switch q
         case 1
             sessiondir = '2021-07-02-17-32-54'; % No GA
@@ -105,7 +105,7 @@ for q = 3:4
         mt = [t(i0(1)) t(i0(end))]; % TODO: this assumes only one motor activation per session
     end
 
-    disp(['session duration: ', num2str(t(end)), ' (n=', num2str(size(t,1)),' samples)']);
+    disp(['session duration: ', num2str(t(end)), ' (n = ', num2str(size(t,1)),' samples)']);
     disp(' ');
 
     % -------------------------------------------------------------------------
@@ -157,10 +157,11 @@ for q = 3:4
     disp(' ');
 
     % -------------------------------------------------------------------------
-    % Outlier detection
+    % Outlier and teleportation detection
     % -------------------------------------------------------------------------
     outlier_th = 300; % values that exceed twice the ~radius (150 pixels) 
-
+    tp_th = 30; % exceeds 10 pixels of movement in 1 frame (about 90 mm/s)
+    
     % magic centroid x,y coordinates
     center = [540 300; 285 300; 210 300; 530 240; 270 260; 200 250];
 
@@ -170,9 +171,12 @@ for q = 3:4
 
     x_fly_outliers = NaN(size(ref_ts,1), sum(n_flies));
     y_fly_outliers = NaN(size(ref_ts,1), sum(n_flies));
+    
+    speed_fly = NaN(size(ref_ts,1), sum(n_flies));
 
     n = 1;
-    disp('% of tracking estimated to be outside the well');
+    artifacts = [];
+    disp('% of tracking estimated to be not available/incorrect');
     for w = 1:n_wells
         for f = 0:(n_flies(w)-1)
             cd([datadir, sessiondir, '/tracking-well-', num2str(w), '/tracking-well-', num2str(w),'_fly', num2str(f)]);
@@ -188,7 +192,7 @@ for q = 3:4
             [~, idx] = intersect(ref_ts, tt);
 
             % replace missing inf values with nan
-            % missing = readNPY('missing.npy'); could use this...
+            missing = readNPY('missing.npy'); % could use this...
             x(x == Inf) = NaN;
             y(y == Inf) = NaN;
 
@@ -200,7 +204,7 @@ for q = 3:4
             % determine x,y coords that fall outside well
     %         center = [nanmedian(x), nanmedian(y)]; % somewhere near the center of the wel
             d = sqrt(nansum(([x y] - center(w,:)).^2, 2));
-            disp(['well ',num2str(w),', fly ',num2str(f),': ', num2str(round(100*(sum(d > outlier_th)/length(d)),1))])
+%             disp(['well ',num2str(w),', fly ',num2str(f),': ', num2str(round(100*(sum(d > outlier_th)/length(d)),1))])
 
     %         figure(n)
     %         plot(x,y,'k'); box off; hold on;
@@ -211,26 +215,51 @@ for q = 3:4
             x_fly_outliers(idx, n) = x;
             y_fly_outliers(idx, n) = y;
 
-            % remove outliers
+            % remove outliers (out of well)
             x(d > outlier_th) = NaN;
             y(d > outlier_th) = NaN;
-
+            
+            % remove potential "teleportations"
+            diff_x = [NaN; diff(x)];
+            diff_y = [NaN; diff(y)];
+            
+            idx_tp = (diff_x >= tp_th | diff_y >= tp_th | diff_x <= -tp_th | diff_y <= -tp_th);
+            x(idx_tp) = NaN;
+            y(idx_tp) = NaN;
+            
             % Add processed x,y coordinates to matrix, align with correct ts
             x_fly(idx, n) = x;
             y_fly(idx, n) = y;
+            
+            % Calculate translational speed (not velocity)
+            speed = [NaN; sqrt(diff(x).^2 + diff(y).^2)];
+            speed_fly(idx, n) = speed;
+            
+            artifacts = [artifacts; 
+                [100*(nansum(missing)/length(x)), ... % occlusion
+                100*(sum(d > outlier_th)/length(x)), ... % out-of-well
+                100*(sum(idx_tp)/length(x)), ... % teleporation
+                100*(sum(isnan(x))/length(x))]]; % total
+            
+            disp(['well ',num2str(w),', fly ',num2str(f)])
+            disp(['    occlusion: ', num2str(100*(nansum(missing)/length(x)))]);
+            disp(['    out-of-well: ', num2str(100*(sum(d > outlier_th)/length(x)))]);
+            disp(['    teleportation: ', num2str(100*(sum(idx_tp)/length(x)))]);
+            disp(['    total: ', num2str(100*(sum(isnan(x))/length(x)))]);
 
             n = n + 1;
         end
     end
     disp(' ');
-
-
+    
+    % Flies to ignore for figures
+    idx_bad = (sum(artifacts(:,2:3),2) > 1)';
+    
     % -------------------------------------------------------------------------
     % Video
     % -------------------------------------------------------------------------
     cd([datadir, sessiondir]);
     for w = 1:n_wells
-        tic
         if ~isfile(['tracking-video-c-well-', num2str(w),'.avi'])
             vr = VideoReader(['video-c-well-', num2str(w),'.mp4']);
             disp(['tracking video-c-well-', num2str(w),'.mp4'])
@@ -315,33 +344,240 @@ for q = 3:4
             close(vw);
             disp(' ');
         end
-        toc
+    end
+    
+  	% -------------------------------------------------------------------------
+    % Conversion
+    % -------------------------------------------------------------------------
+    % convert from pixels per frame to mm per second
+    
+    % width of the well in pixels (well is 30 mm)
+    width_well = [327 345 322 324 345 318]; % magic numbers from video
+    for w = 1:n_wells
+        
+        % pixels/frame * frames/second * mm/pixel
+        speed_fly(:, idx_well(w,1):idx_well(w,2)) = speed_fly(:, idx_well(w,1):idx_well(w,2)).*(frame_rate*(30./width_well(w)));
+        
+        % pixels * mm/pixel
+        x_fly(:, idx_well(w,1):idx_well(w,2)) = x_fly(:, idx_well(w,1):idx_well(w,2)).*(30./width_well(w));
+        y_fly(:, idx_well(w,1):idx_well(w,2)) = y_fly(:, idx_well(w,1):idx_well(w,2)).*(30./width_well(w));
+    end
+    center = center.*(30./width_well)';
+    
+    
+    % -------------------------------------------------------------------------
+    % Filter
+    % -------------------------------------------------------------------------
+    % All frequency values are in Hz.
+    Fs = frame_rate;  % Sampling Frequency
+    
+    % Attenuate everything under 0.1 Hz = 5 to 10 Seconds 
+    Fpass = 0.1;              % Passband Frequency
+    Fstop = 0.2;              % Stopband Frequency
+    Dpass = 0.0057563991496;  % Passband Ripple
+    Dstop = 0.0001;           % Stopband Attenuation
+    dens  = 20;               % Density Factor
+
+    % Calculate the order from the parameters using FIRPMORD.
+    [N, Fo, Ao, W] = firpmord([Fpass, Fstop]/(Fs/2), [1 0], [Dpass, Dstop]);
+
+    % Calculate the coefficients using the FIRPM function.
+    filter_coeff  = firpm(N, Fo, Ao, W, {dens});
+
+    % -------------------------------------------------------------------------
+    % Figures
+    % -------------------------------------------------------------------------
+    % Translational speed or angular speed 1D time-plots per fly or grouped by
+    % well, or grouped by session
+    
+    % Per well
+    figure(1);
+    for w = 1:n_wells
+        % good flies for that well
+        idx = intersect(find(~idx_bad), idx_well(w,1):idx_well(w,2));
+        
+        if isempty(idx)
+        else
+            plot_speed = nanmean(speed_fly(:,idx),2);
+            plot_speed(isnan(plot_speed)) = 0;
+            plot_speed = filtfilt(filter_coeff, 1, plot_speed);
+            plot_speed(plot_speed < 0) = 0;
+
+            subplot(3, 2, w)
+            box off; hold on;
+            plot(ref_ts, nanmean(speed_fly(:,idx),2), 'k');
+            plot(ref_ts, plot_speed, 'r');
+            line(mt, [10 10],'LineWidth',2, 'Color','g')
+            plot(mt(1),10,'gs','MarkerFaceColor','g')
+            plot(mt(2),10,'gs','MarkerFaceColor','g')
+            xlim([300 ref_ts(end)]);
+            ylim([0 20]);
+            xlabel('Time (s)');
+            ylabel('Speed (mm/s)');
+            title(['Well ', num2str(w),' (n = ', num2str(length(idx)), ')']);
+        end
+    end
+    
+    % All flies
+    figure(2)
+    subplot(1,2,1);
+    plot_speed = nanmean(speed_fly(:,~idx_bad),2);
+    plot_speed(isnan(plot_speed)) = 0;
+    plot_speed = filtfilt(filter_coeff, 1, plot_speed);
+    plot_speed(plot_speed < 0) = 0;
+                
+    plot(ref_ts, nanmean(speed_fly(:,~idx_bad),2), 'k'); box off; hold on;
+    plot(ref_ts, plot_speed, 'r');
+    line(mt, [10 10],'LineWidth',2, 'Color','g')
+    plot(mt(1),10,'gs','MarkerFaceColor','g')
+    plot(mt(2),10,'gs','MarkerFaceColor','g')
+    xlim([300 ref_ts(end)]);
+    ylim([0 20]);
+    xlabel('Time (s)');
+    ylabel('Speed (mm/s)');
+    title('All flies');
+    
+    subplot(1,2,2);
+    if sum(isnan(st(:))) > 0
+        bar([nanmean(nanmean(speed_fly((ref_ts > 300 & ref_ts < mt(1)), ~idx_bad), 2)), ... % Baseline
+        nanmean(nanmean(speed_fly(ref_ts > mt(2),~idx_bad), 2))]); % motor to end GA
+        box off;
+        ylabel('Mean speed (mm/s)');
+        set(gca,'XTick',1:2,'XTickLabel',{'Base','Motor'})
+    else
+        bar([nanmean(nanmean(speed_fly((ref_ts > 300 & ref_ts < st(1,2)), ~idx_bad), 2)), ... % Baseline
+        nanmean(nanmean(speed_fly((ref_ts > st(2,1) & ref_ts < mt(1)),~idx_bad), 2)), ... % start GA to motor
+        nanmean(nanmean(speed_fly((ref_ts > mt(2) & ref_ts < st(2,2)),~idx_bad), 2)), ... % motor to end GA
+        nanmean(nanmean(speed_fly((ref_ts > st(3,1)),~idx_bad), 2))]); % Recovery
+        box off;
+        ylabel('Mean speed (mm/s)');
+        set(gca,'XTick',1:4,'XTickLabel',{'Base','GA','Motor','Recovery'})
     end
 
+    % 2D movement plot showing one fly or multiple flies (by color) for some
+    % shorter (5 minutes?) of activity
+    figure(3);
+    n = 1;
+    pad = 20;
+    for w = 1:3 % magic number
+        idx = intersect(find(~idx_bad), idx_well(w,1):idx_well(w,2));
+        
+        if isempty(idx)
+        else
+            % Baseline
+            subplot(3,4,n);
+            plot(x_fly(ref_ts > st(1,2)-300 & ref_ts < st(1,2),idx), y_fly(ref_ts > st(1,2)-300 & ref_ts < st(1,2),idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            ylabel(['Well ', num2str(w),' (n = ', num2str(length(idx)), ')']);
+            title('Baseline');
+            
+            % GA (before motor)
+            subplot(3,4,n+1);
+            plot(x_fly(ref_ts > st(2,2)-600 & ref_ts < st(2,2)-300,idx), y_fly(ref_ts > st(2,2)-600 & ref_ts < st(2,2)-300,idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Sevoflurane (before motor)');
+            
+            % GA (after motor)
+            subplot(3,4,n+2);
+            plot(x_fly(ref_ts > st(2,2)-300 & ref_ts < st(2,2),idx), y_fly(ref_ts > st(2,2)-300 & ref_ts < st(2,2),idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Sevoflurane (after motor)');
+            
+            % Recovery
+            subplot(3,4,n+3);
+            plot(x_fly(ref_ts > st(3,1)+600 & ref_ts < st(3,1)+900,idx), y_fly(ref_ts > st(3,1)+600 & ref_ts < st(3,1)+900,idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Recovery');
+            
+            n = n + 4;
+        end
+    end
+    
+    
+    figure(4);
+    n = 1;
+    for w = 4:6 % magic number
+        idx = intersect(find(~idx_bad), idx_well(w,1):idx_well(w,2));
+        
+        if isempty(idx)
+        else
+            % Baseline
+            subplot(3,4,n);
+            plot(x_fly(ref_ts > st(1,2)-300 & ref_ts < st(1,2),idx), y_fly(ref_ts > st(1,2)-300 & ref_ts < st(1,2),idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            ylabel(['Well ', num2str(w),' (n = ', num2str(length(idx)), ')']);
+            title('Baseline');
+            
+            % GA (before motor)
+            subplot(3,4,n+1);
+            plot(x_fly(ref_ts > st(2,2)-600 & ref_ts < st(2,2)-300,idx), y_fly(ref_ts > st(2,2)-600 & ref_ts < st(2,2)-300,idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Sevoflurane (before motor)');
+            
+            % GA (after motor)
+            subplot(3,4,n+2);
+            plot(x_fly(ref_ts > st(2,2)-300 & ref_ts < st(2,2),idx), y_fly(ref_ts > st(2,2)-300 & ref_ts < st(2,2),idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Sevoflurane (after motor)');
+            
+            % Recovery
+            subplot(3,4,n+3);
+            plot(x_fly(ref_ts > st(3,1)+600 & ref_ts < st(3,1)+900,idx), y_fly(ref_ts > st(3,1)+600 & ref_ts < st(3,1)+900,idx)); box off;
+            i0 = x_fly(:,idx);
+            i0 = nanmean(i0(:));
+            xlim([i0-pad i0+pad]);
+            i1 = y_fly(:,idx);
+            i1 = nanmean(i1(:));
+            ylim([i1-pad i1+pad]);
+            title('Recovery');   
+            
+            n = n + 4;
+        end
+    end
+    
+    % 2D heatmap by well for some timepoints
+
+    % Scatter plot of individual mean fly speed for baseline vs GA
+
+    % Dose-response curve across sessions for movement during GA
+    
+    % Histogram or CDF of individual (or group) fly speeds
 end
-% -------------------------------------------------------------------------
-% Figures
-% -------------------------------------------------------------------------
-% Translational speed or angular speed 1D time-plots per fly or grouped by
-% well, or grouped by session
-
-% Histogram or CDF of individual (or group) fly speeds
-
-% 2D movement plot showing one fly or multiple flies (by color) for some
-% shorter (5 minutes?) of activity
-
-% 2D heatmap by well for some timepoints
-
-% Scatter plot of individual mean fly speed for baseline vs GA
-
-% Example video with overlay or just overlay alone
-
-% Dose-response curve across sessions for movement during GA
-
-
-
-
-
 
 
 
