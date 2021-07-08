da2255ba57ef01310c4475a05bab8cfe6adb7d8f
Jon Kenny <jkenny@caltech.edu>
2021-07-07 17:27:01 -0700

    Added selfcheck

--- experiment.sh
+++ experiment.sh
@@ -2,8 +2,9 @@
 
 # Experiment
 echo EXPERIMENT
-# /usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 10 --n_flies 0 # Test exp.
-/usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 90 --n_flies 2 --t_motor_on 20 40  --motor_duration 10 10
+# /usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 300 --n_flies 0 0 0 0 0 0 # Test exp
+# /usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 30 --n_flies 0 0 0 0 0 0 --t_motor_on 15 --motor_duration 10
+/usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 4800 --n_flies 0 0 2 1 2 2 --t_motor_on 2700 --motor_duration 5
 # /usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 5400 --n_flies 6 --t_motor_on 1800 3600  --motor_duration 10 10
 
 # Transfer to Synology
@@ -13,4 +14,4 @@ echo
 
 # Run "remote" analysis
 echo ANALYSIS
-/usr/bin/python3 /home/pi/recording/analysis/process_data.py
+# /usr/bin/python3 /home/pi/recording/analysis/process_data.py
--- selfcheck.sh
+++ selfcheck.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-/usr/bin/python3 startup.py --selfcheck True --t_experiment 120 --t_motor_on 10 30 50 70 90 --motor_duration 10 10 10 10 10
+# Experiment
+echo EXPERIMENT
+/usr/bin/python3 startup.py --datadir /media/pi/Elements/anesthesia-reanimation/data/ --t_experiment 20 --n_flies 0 0 0 0 0 0 --t_motor_on 10 --motor_duration 5
+
+# Transfer to Synology
+echo TRANSFER
+bash /home/pi/recording/transfer_data.sh
+echo
+
+# Run "remote" analysis
+echo ANALYSIS
+# /usr/bin/python3 /home/pi/recording/analysis/process_data.py
